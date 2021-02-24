bae4071fab55498760babcdb63ae125091bbe3cc
Nathan Ricks <54324815+ricksnp@users.noreply.github.com>
2021-02-24 14:40:04 -0500

    Finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import './App.css';
+import Home from './views/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './views/Services';
+import Products from './views/Products';
+import SignUp from './views/SignUp';
 
 function App() {
 	return (
@@ -8,7 +13,10 @@ function App() {
 			<Router>
 				<Navbar />
 				<Switch>
-					<Route path="/" exact />
+					<Route path="/" exact component={Home} />
+					<Route path="/services" component={Services} />
+					<Route path="/products" component={Products} />
+					<Route path="/sign-up" component={SignUp} />
 				</Switch>
 			</Router>
 		</>
