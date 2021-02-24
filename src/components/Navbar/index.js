98d3c1603e4cf38aa9653d1f141fd08b266b13e1
Nathan Ricks <54324815+ricksnp@users.noreply.github.com>
2021-02-24 08:18:28 -0500

    Sign up / Nav styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../Button';
 import './Navbar.css';
@@ -9,6 +9,20 @@ function Navbar() {
 
 	const handleClick = () => setClick(!click);
 	const closeMobileMenu = () => setClick(false);
+
+	const showButton = () => {
+		if (window.innerWidth <= 960) {
+			setButton(false);
+		} else {
+			setButton(true);
+		}
+	};
+
+	useEffect(() => {
+		showButton();
+	}, []);
+
+	window.addEventListener('resize', showButton);
 	return (
 		<>
 			<nav className="navbar">
